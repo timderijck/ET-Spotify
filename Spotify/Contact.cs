0a95ebabaca1956daf79a2899bf3f55d3b366f86
timderijck <timderijck2@gmail.com>
2026-06-04 11:11:39 +0200

    Bij input 1 geeft hij contactinfo weer

---
+++ Spotify/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Playlist
+{
+    class Playlist
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
