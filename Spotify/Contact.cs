bbcc5d59b4dd3517174f54cc0834d4a9f083ccd8
timderijck <timderijck2@gmail.com>
2026-06-04 11:11:58 +0200

    Push naar branch

--- Spotify/Contact.cs
+++ Spotify/Contact.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Playlist
+namespace Spotify
 {
-    class Playlist
+    class Contact
     {
-        static void Main(string[] args)
+        public static void ContactText()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" ===  Contactpagina  === ");
+            Console.WriteLine("Is er iets aan de hand? Wij kunnen je helpen.");
+            Console.WriteLine("1. E-mail: etspotify@mail.com");
+            Console.WriteLine("1. Telefoonnummer: +31 6 12345678");
         }
     }
 }
---
+++ Spotify/Mainpagina.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotify
+{
+    internal class Mainpagina
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ===  Hoofdpagina  === ");
+            Console.WriteLine(" ===  Wat wilt u graag doen?  === ");
+
+            string keuze = Console.ReadLine();
+
+            if (keuze == "1")
+            {
+                Contact.ContactText();
+            }
+            else if (keuze == "2")
+            {
+                Console.WriteLine("Contact");
+            }
+            else if (keuze == "3")
+            {
+                Console.WriteLine("Instellingen");
+            }
+            else
+            {
+                Console.WriteLine("Ongeldige keuze");
+            }
+        }
+    }
+}
