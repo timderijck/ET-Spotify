8745bbc50dee3eb1e94912d28a04addd86f0b466
timderijck <timderijck2@gmail.com>
2026-06-04 13:24:06 +0200

    vergeten 3. te zetten

--- Spotify/Contact.cs
+++ Spotify/Contact.cs
@@ -12,7 +12,7 @@ namespace Spotify
             Console.WriteLine("Is er iets aan de hand? Wij kunnen je helpen.");
             Console.WriteLine("\n 1. E-mail: etspotify@mail.com");
             Console.WriteLine("2. Telefoonnummer: +31 6 12345678");
-            Console.WriteLine("Voor besturing, gebruik de pijltjes en Enter");
+            Console.WriteLine("3. Voor besturing, gebruik de pijltjes en Enter");
         }
     }
 }
